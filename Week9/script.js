4e7c9ae52c629deaa337e2ec39168b60fbf87a5a
Epric096 <eh22og@brocku.ca>
2024-03-20 22:47:46 -0400

    Add data vis

--- Week9/script.js
+++ Week9/script.js
@@ -71,24 +71,32 @@ renderer.setSize(sizes.width, sizes.height)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
+/***********
+ ** LIGHT **
+ ***********/
+// Directional Light
+const directionalLight = new THREE.DirectionalLight(0x404040, 100)
+scene.add(directionalLight)
+
 /************
  ** MESHES **
  ************/
+
 // Cube Geometry
-const cubeGeometry = new THREE.BoxGeometry(2, 2, 2)
+const cubeGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
 
 // Cube Materials
-const redMaterial = new THREE.MeshBasicMaterial(
+const redMaterial = new THREE.MeshStandardMaterial(
     {
         color: new THREE.Color('red')
     }
 )
-const greenMaterial = new THREE.MeshBasicMaterial(
+const greenMaterial = new THREE.MeshStandardMaterial(
     {
         color: new THREE.Color('green')
     }
 )
-const blueMaterial = new THREE.MeshBasicMaterial(
+const blueMaterial = new THREE.MeshStandardMaterial(
     {
         color: new THREE.Color('blue')
     }
@@ -97,15 +105,44 @@ const blueMaterial = new THREE.MeshBasicMaterial(
 const drawCube = (i, material) =>
 {
     const cube = new THREE.Mesh(cubeGeometry, material)
-    cube.position.set(i,0,0)
+    cube.position.x = (Math.random() - 0.5) * 10
+    cube.position.z = (Math.random() - 0.5) * 10
+    cube.position.y = i - 10
+
+    cube.rotation.x = Math.random() * 2 * Math.PI
+    cube.rotation.y = Math.random() * 2 * Math.PI
+    cube.rotation.z = Math.random() * 2 * Math.PI
+
     scene.add(cube)
 }
 
-/******************
- ** TEXT PARSERS **
- ******************/
+/***********************
+ ** TEXT PARSERS & UI **
+ ***********************/
+ let preset = {}
+
 const uiobj = {
-    text: ''
+    text: '',
+    textArray: [],
+    term1: 'cupboard',
+    term2: 'hat',
+    term3: 'broom',
+    rotateCamera: false,
+    reveal(){
+        // Save terms to uiobj
+        preset = ui.save()
+
+        // Parse Text and Terms
+        parseTextandTerms()
+
+        // Hide termsFolder Ui
+        termsFolder.hide()
+
+        // Show interactionFolder ui
+        createInteractionFolders()
+        
+
+    }
 }
 
 // Text Parsers
@@ -116,7 +153,6 @@ fetch("https://raw.githubusercontent.com/amephraim/nlp/master/texts/J.%20K.%20Ro
         (data) =>
         {
             uiobj.text = data
-            parseTextandTerms()
         }
     )
 
@@ -126,9 +162,85 @@ const parseTextandTerms = () =>
     // strip periods and downcase text
     const parsedText = uiobj.text.replaceAll(".", "").toLowerCase()
 
+    // Tokenize text
+    uiobj.textArray = parsedText.split(/[^\w']+/)
+
+    // Find term 1
+    findTermInParsedText(uiobj.term1, redMaterial)
+
+    // Find term 2
+    findTermInParsedText(uiobj.term2, greenMaterial)
+
+    // Find term 3
+    findTermInParsedText(uiobj.term3, blueMaterial)
+
 }
 
-parseTextandTerms()
+const findTermInParsedText = (term, material) =>
+{
+    for (let i = 0; i < uiobj.textArray.length; i++)
+    {
+        if(uiobj.textArray[i] === term)
+        {
+            // convert i into n, which is a value between 0 and 20
+            const n = (100 / uiobj.textArray.length) * i * 0.2
+            
+            // call drawCube function 5 times using converted n value
+            for(let a = 0; a < 5; a++)
+            {
+                drawCube(n, material)
+            }
+        }
+    }
+}
+
+// UI
+const ui = new dat.GUI()
+
+// Terms Folder
+const termsFolder = ui.addFolder('Enter Terms')
+
+termsFolder
+    .add(uiobj, 'term1')
+    .name('Red Term')
+
+termsFolder
+    .add(uiobj, 'term2')
+    .name('Green Term')
+
+termsFolder
+    .add(uiobj, 'term3')
+    .name('Blue Term')
+
+termsFolder
+    .add(uiobj, 'reveal')
+    .name('Reveal')
+
+// Interaction Folder
+const createInteractionFolders = () =>
+{
+    // Cubes Folder
+    const cubesFolder = ui.addFolder('Filter Term')
+
+    cubesFolder
+        .add(redMaterial, 'visible')
+        .name(`${uiobj.term1}`)
+    
+    cubesFolder
+        .add(blueMaterial, 'visible')
+        .name(`${uiobj.term2}`)
+    
+    cubesFolder
+        .add(greenMaterial, 'visible')
+        .name(`${uiobj.term3}`)
+
+    // Camera Folder
+    const cameraFolder = ui.addFolder('Camera')
+    
+    cameraFolder
+        .add(uiobj, 'rotateCamera')
+        .name('Rotate Camera')
+}
 /***************
  ** ANIMATION **
  ***************/
@@ -142,6 +254,13 @@ const animation = () =>
 
     // Orbit Controls
     controls.update()
+
+    // Camera Rotation
+    if(uiobj.rotateCamera)
+    {
+        camera.position.x = Math.sin(elapsedTime * 0.2) * 16
+        camera.position.z = Math.cos(elapsedTime * 0.2)  * 16
+    }
 
     // Renderer
     renderer.render(scene, camera)
